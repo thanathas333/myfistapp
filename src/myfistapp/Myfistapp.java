df6d2715143aebc6e00a622a9e5611a528281768
thanathas_333@hotmail.com <>
2014-05-14 09:13:30 +0700

    สร้างโปรเจค

---
+++ src/myfistapp/Myfistapp.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package myfistapp;
+
+/**
+ *
+ * @author Administrator
+ */
+public class Myfistapp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.println("Vampire sam"); // TODO code application logic here
+    }
+    
+}
